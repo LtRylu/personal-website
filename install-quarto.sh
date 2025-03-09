a100ba162933a15aa0dad2eccb79020d1f798114
Ryan Tolone <iiryantoloneii@gmail.com>
2025-03-09 00:27:47 -0800

    updates

--- install-quarto.sh
+++ install-quarto.sh
@@ -2,6 +2,11 @@
 
 # Download and install Quarto
 QUARTO_VERSION="1.3.450"
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb
-sudo dpkg -i quarto-${QUARTO_VERSION}-linux-amd64.deb
-rm quarto-${QUARTO_VERSION}-linux-amd64.deb
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
+tar -xvzf quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
+sudo mv quarto-${QUARTO_VERSION} /opt/quarto
+sudo ln -s /opt/quarto/bin/quarto /usr/local/bin/quarto
+rm quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
+
+# Verify installation
+quarto --version
