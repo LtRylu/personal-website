f3ca0057b45036afe8eaf490f20955a4e24e0542
Ryan Tolone <iiryantoloneii@gmail.com>
2025-03-09 00:32:53 -0800

    1232

--- install-quarto.sh
+++ install-quarto.sh
@@ -4,8 +4,15 @@
 QUARTO_VERSION="1.3.450"
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
 tar -xvzf quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
-sudo mv quarto-${QUARTO_VERSION} /opt/quarto
-sudo ln -s /opt/quarto/bin/quarto /usr/local/bin/quarto
+
+# Create directory and move only necessary files
+sudo mkdir -p /opt/quarto
+sudo mv quarto-${QUARTO_VERSION}/bin/quarto /opt/quarto/
+sudo mv quarto-${QUARTO_VERSION}/bin/tools /opt/quarto/
+sudo ln -s /opt/quarto/quarto /usr/local/bin/quarto
+
+# Clean up
+rm -rf quarto-${QUARTO_VERSION}
 rm quarto-${QUARTO_VERSION}-linux-amd64.tar.gz
 
 # Verify installation
